1cb047e15ea1f0283dd9971cc8c978da92c03c40
Ferris <747852289@qq.com>
2021-07-05 10:34:29 +0800

    Initial Commit

---
+++ Example-XCFramework/Example_XCFramework.h
@@ -0,0 +1,18 @@
+//
+//  Example_XCFramework.h
+//  Example-XCFramework
+//
+//  Created by Fozu on 2021/7/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Example_XCFramework.
+FOUNDATION_EXPORT double Example_XCFrameworkVersionNumber;
+
+//! Project version string for Example_XCFramework.
+FOUNDATION_EXPORT const unsigned char Example_XCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Example_XCFramework/PublicHeader.h>
+
+
